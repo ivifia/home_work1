948c041ba1e8bd15b2e331335afb26a2a6c72d46
bakuk aleksandr <bakuksasha@gmail.com>
2024-10-20 19:53:16 +0700

    bug fix

--- str_func.py
+++ str_func.py
@@ -3,7 +3,9 @@ def up_string(string):
     return string.upper()
 def upping_first_letters(text):
     """Функция для заглавных букв"""
+    """Исправление бага"""
     return ' '.join(word.capitalize() for word in text.split())
+
 
 
 
