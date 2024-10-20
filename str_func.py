4584baf9dbd50384420910c5dcf46ecec5265799
bakuk aleksandr <bakuksasha@gmail.com>
2024-10-20 19:40:28 +0700

    editing function

--- str_func.py
+++ str_func.py
@@ -1,2 +1,9 @@
 def up_string(string):
+    """Возвращение строки в верхнем регистре"""
     return string.upper()
+def upping_first_letters(text):
+    """Функция для заглавных букв"""
+    return ' '.join(word.capitalize() for word in text.split())
+
+
+
